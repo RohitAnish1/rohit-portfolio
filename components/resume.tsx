e6c2fc17f788e0803de55905da5e6e4b8f5bc729
Rohit Anish <rohitanish158@gmail.com>
2025-11-12 10:04:53 +0530

    changed resume link

--- components/resume.tsx
+++ components/resume.tsx
@@ -47,7 +47,7 @@ export default function Resume() {
 
               <div className="flex flex-col sm:flex-row gap-4">
                 <motion.a
-                  href="https://drive.google.com/file/d/1z-_v9dd6uPkF8q_6QDlGTP2tgj7fhUrY/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1R3PXud6PtRKTfjlN_qe6B_gJ4vNJWsre/view?usp=drive_link"
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
                   className="px-6 py-3 rounded-full bg-gray-700 text-white font-medium hover:bg-gray-600 transition-all duration-300 flex items-center justify-center"
